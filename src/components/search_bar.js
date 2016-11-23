044dbd8e0cc6783d029196d533d707b92cf8c07f
Clay Kohut <claykohut@MacBook-Pro-2.local>
2016-11-23 16:22:40 -0500

    value set by this.state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,9 @@ class SearchBar extends Component {
     console.log('rendering SearchBar..')
     return (
       <div>
-        <input onChange={ (event) => this.setState({ term: event.target.value }) } />
+        <input
+          value={this.state.term}
+          onChange={ (event) => this.setState({ term: event.target.value }) } />
       </div>
     )
   }
