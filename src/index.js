14b7b90e99439f7af380c8a5c847f907d468a7f9
Clay Kohut <claykohut@MacBook-Pro-2.local>
2016-11-23 17:36:21 -0500

    got video in place on load

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyCS9XWzgowdxiWXbiuITvg5xWo7BM6C6bI'
 
@@ -17,13 +18,13 @@ class App extends Component {
 			console.log('got yt data ', videos)
 			this.setState({ videos });
 		})
-
 	}
 
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={ this.state.videos[0] }/>
 				<VideoList videos={ this.state.videos } />
 			</div>
 		);
